7df5efffbc678c3f4bdbb167cd339a6af22df252
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-11-22 14:40:30 +0300

    First Trial

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
-// Your code here
+function pointsForWord(word) {
+    let points = 0;
+    console.log(word.split(""))
+
+    word.split("").forEach(letter => {
+        if (["a", "e", "i", "o", "u"].includes(letter.toLowerCase())) {
+            points += 1;
+        } else {
+            points += 2;
+        }
+    })
+
+    return points;
+}
+
+export { pointsForWord };
